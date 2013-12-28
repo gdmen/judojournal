36ba13cfed1f82b07040e0488bef7313ad21f5a7
unknown <gmenezes@seas.upenn.edu>
2013-12-28 00:15:10 -0500

    Permission checks added to all models.

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,18 +1,6 @@
 from rest_framework import viewsets
 from apps.api.serializers import *
 
-class PermissionViewSet(viewsets.ModelViewSet):
-  queryset = Permission.objects.all()
-  serializer_class = PermissionSerializer
-
-class GroupViewSet(viewsets.ModelViewSet):
-  queryset = Group.objects.all()
-  serializer_class = GroupSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
-
 class HasUserViewSet(viewsets.ModelViewSet):
   model = None
   def pre_save(self, obj):
@@ -24,22 +12,46 @@ class HasUserViewSet(viewsets.ModelViewSet):
   class Meta:
     abstract = True
 
+class PermissionViewSet(viewsets.ModelViewSet):
+  model = Permission
+  def get_queryset(self):
+    if self.request.user.is_superuser:
+      return self.model.objects.all()
+    return []
+  serializer_class = PermissionSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+  model = Group
+  def get_queryset(self):
+    if self.request.user.is_superuser:
+      return self.model.objects.all()
+    return []
+  serializer_class = GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+  model = User
+  def get_queryset(self):
+    if self.request.user.is_superuser:
+      return self.model.objects.all()
+    return self.model.objects.filter(id=self.request.user.id)
+  serializer_class = UserSerializer
+
 class LocationViewSet(HasUserViewSet):
   model = Location
   serializer_class = LocationSerializer
 
-class GoalViewSet(viewsets.ModelViewSet):
-  queryset = Goal.objects.all()
+class GoalViewSet(HasUserViewSet):
+  model = Goal
   serializer_class = GoalSerializer
 
-class QuestionViewSet(viewsets.ModelViewSet):
-  queryset = Question.objects.all()
+class QuestionViewSet(HasUserViewSet):
+  model = Question
   serializer_class = QuestionSerializer
 
-class StatusViewSet(viewsets.ModelViewSet):
-  queryset = Status.objects.all()
+class StatusViewSet(HasUserViewSet):
+  model = Status
   serializer_class = StatusSerializer
 
-class EventViewSet(viewsets.ModelViewSet):
-  queryset = Event.objects.all()
+class EventViewSet(HasUserViewSet):
+  model = Event
   serializer_class = EventSerializer
