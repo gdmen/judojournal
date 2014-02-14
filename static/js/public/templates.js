be3c795c9a758efa683d55467d3e1ee0f773e833
unknown <gmenezes@seas.upenn.edu>
2014-02-13 20:48:18 -0500

    Entry header styling.

--- static/js/public/templates.js
+++ static/js/public/templates.js
@@ -76,16 +76,16 @@ function program8(depth0,data) {
   return buffer;
   }
 
-  buffer += "\r\n<div class=\"entry public\">\r\n  <div class=\"row\">\r\n    <div id=\"entry-rating\">\r\n      ";
+  buffer += "\r\n<div class=\"entry public\">\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"columns entry-header\">\r\n    \r\n      <div id=\"entry-rating\">\r\n        ";
   stack1 = self.invokePartial(partials['partials/rating'], 'partials/rating', depth0, helpers, partials, data);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\r\n    </div>\r\n  </div>\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"columns entry-header\">\r\n    \r\n      <h3 id=\"art\" class=\"headertext\">"
+  buffer += "\r\n      </div>\r\n    \r\n      <h3 id=\"art\" class=\"headertext\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.art)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</h3>\r\n      \r\n      <h3 id=\"type\" class=\"headertext\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.type)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</h3>\r\n      \r\n      <h3 id=\"location\" class=\"headertext\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.location)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h3>\r\n      \r\n      <div>\r\n        <h3 class=\"\">\r\n          <i class=\"fa fa-calendar\"></i>\r\n          Thursday, 15 Jan 2014\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">from</h3>\r\n        <h3 class=\"\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          2:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">until</h3>\r\n        <h3 class=\"\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          4:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n    </div>\r\n  </div>\r\n\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"large-6 columns\">\r\n      <div class=\"module-section drill\">\r\n        <div class=\"section-header\">\r\n          <span class=\"list-title\">Drills</span>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
+    + "</h3>\r\n      \r\n      <div>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-calendar\"></i>\r\n          Thursday, 15 Jan 2014\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">from</h3>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          2:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">until</h3>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          4:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n    </div>\r\n  </div>\r\n\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"large-6 columns\">\r\n      <div class=\"module-section drill\">\r\n        <div class=\"section-header\">\r\n          <span class=\"list-title\">Drills</span>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.drills), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\r\n        </ul>\r\n      </div>\r\n    </div>\r\n\r\n    <div class=\"large-6 columns\">\r\n      <div class=\"module-section sparring\">\r\n        <div class=\"section-header\">\r\n          <span class=\"list-title\">Sparring</span>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
