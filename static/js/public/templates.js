abd91e9db780d37ea600b2410dc5cc032f4826e5
unknown <gmenezes@seas.upenn.edu>
2014-02-14 16:41:11 -0500

    Entry view styling.

--- static/js/public/templates.js
+++ static/js/public/templates.js
@@ -91,16 +91,16 @@ function program10(depth0,data) {
   return buffer;
   }
 
-  buffer += "\r\n<div class=\"entry public\">\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"columns entry-header\">\r\n    \r\n      <div id=\"entry-rating\">\r\n        ";
+  buffer += "\r\n<div class=\"entry public\">\r\n\r\n  <div class=\"row\">\r\n    <div class=\"columns entry-header\">\r\n      <div class=\"entry-header-left\">\r\n        <div class=\"date\">\r\n          <h6 class=\"month\">February</h6>\r\n          <span class=\"day\">14</span>\r\n          <span class=\"year\">2014</span>\r\n        </div>\r\n      </div>\r\n      \r\n      <div id=\"entry-rating\">\r\n        ";
   stack1 = self.invokePartial(partials['partials/rating'], 'partials/rating', depth0, helpers, partials, data);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\r\n      </div>\r\n    \r\n      <h3 id=\"art\" class=\"headertext\">"
+  buffer += "\r\n      </div>\r\n      \r\n      <div class=\"entry-header-row\">\r\n        <h1 id=\"art\" class=\"headertext\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.art)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h3>\r\n      \r\n      <h3 id=\"type\" class=\"headertext\">"
+    + "</h1>\r\n        <h1 id=\"type\" class=\"headertext\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.type)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h3>\r\n      \r\n      <h3 id=\"location\" class=\"headertext\">"
+    + "</h1>\r\n      </div>\r\n      \r\n      <div class=\"entry-header-section\">\r\n      \r\n        <div class=\"entry-header-row\">\r\n          <h5 id=\"location\" class=\"headertext\">gdm @ "
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.location)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h3>\r\n    </div>\r\n      \r\n    <div class=\"columns entry-header\">\r\n      <div>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-calendar\"></i>\r\n          Thursday, 15 Jan 2014\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">from</h3>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          2:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n      <div>\r\n        <h3 class=\"plaintext\">until</h3>\r\n        <h3 class=\"headertext\">\r\n          <i class=\"fa fa-clock-o\"></i>\r\n          4:45PM\r\n        </h3>\r\n      </div>\r\n      \r\n    </div>\r\n  </div>\r\n\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"medium-10 large-uncentered large-6 xlarge-4 columns\">\r\n      <div class=\"module-section drill\">\r\n        <div class=\"section-header\">\r\n          <h3 class=\"list-title\">Drills</h3>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
+    + "</h5>\r\n        </div>\r\n        \r\n        <div class=\"entry-header-row\">\r\n          <h5 class=\"headertext\">\r\n            2:45PM - 4:45PM (2 hours)\r\n          </h5>\r\n        </div>\r\n        \r\n      </div>\r\n\r\n    </div>\r\n  </div>\r\n\r\n\r\n  <div class=\"row\">\r\n\r\n    <div class=\"medium-10 large-uncentered large-6 xlarge-4 columns\">\r\n      <div class=\"module-section drill\">\r\n        <div class=\"section-header\">\r\n          <h3 class=\"list-title\">Drills</h3>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.drills), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\r\n        </ul>\r\n      </div>\r\n    </div>\r\n\r\n    <div class=\"medium-10 large-uncentered large-6 xlarge-4 columns\">\r\n      <div class=\"module-section sparring\">\r\n        <div class=\"section-header\">\r\n          <h3 class=\"list-title\">Sparring</h3>\r\n        </div>\r\n        <ul class=\"model-list row\">\r\n          ";
