a9db1cd4cbc77424ee12244f08da5a2bf21b0d6e
unknown <gmenezes@seas.upenn.edu>
2014-02-13 23:10:29 -0500

    Deploy script update.

--- bluehost.sh
+++ bluehost.sh
@@ -1,3 +1,4 @@
+yes | rm -rf ~/public_html/judojournal/static
 yes | cp -rf static/ ~/public_html/judojournal/
 yes | cp public_html/* ~/public_html/judojournal/
 chmod 0755 ~/public_html/judojournal/index.fcgi
