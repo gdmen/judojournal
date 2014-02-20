37202082f02ca0dde4dd5931d10df33a321130fe
unknown <gmenezes@seas.upenn.edu>
2014-02-19 23:56:49 -0500

    Profile updates.

--- static/js/private/views.js
+++ static/js/private/views.js
@@ -121,7 +121,6 @@ JJ.Views.AbstractEditModel = JJ.Views.AbstractView.extend({
 		this.selectors = {};
 		this.selectors["save"] = "#" + this.model.cid + "-save";
 		this.baseEvents["click " + this.selectors.save + ".enabled"] = "save";
-    console.log(this.model);
 		this.model.on("change", this.enableSave, this);
 		// TODO - fix where this is used. It's weird.
 		this.model.parentView = this;
@@ -662,6 +661,10 @@ JJ.Views.EditJudoEntry = JJ.Views.AbstractEditModel.extend({
 			defaultDate: null,
 		});
 		this.dateEl.datepicker("setDate", this.model.get("start"));
+    this.$el.find(".entry-save").scrollToFixed({
+      preFixed: function() { $(this).css('margin-top', '1rem'); },
+      postFixed: function() { $(this).css('margin-top', ''); }
+    });
   },
   
   render: function() {
